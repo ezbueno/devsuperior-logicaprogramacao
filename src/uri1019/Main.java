4f1e9671a52ba94d317145e05305dc8acf9ebb8e
ezbueno <ezandrobueno@yahoo.com.br>
2021-12-20 14:59:12 -0300

    Exercícios resolvidos

---
+++ src/uri1019/Main.java
@@ -0,0 +1,18 @@
+package uri1019;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			int N = sc.nextInt();
+			
+			int horas = N / 3600;
+			int minutos = (N % 3600) / 60;
+			int segundos = N % 60;
+			
+			System.out.println(horas + ":" + minutos + ":" + segundos);
+		}
+	}
+}
