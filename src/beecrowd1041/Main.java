d40ca8fa61a055a1574cc77720f695895515ca57
ezbueno <ezandrobueno@yahoo.com.br>
2021-12-28 14:43:47 -0300

    Exercícios resolvidos

--- src/beecrowd1041/Main.java
+++ src/beecrowd1041/Main.java
@@ -9,7 +9,24 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		
 		try (Scanner sc = new Scanner(System.in)) {
+			double x = sc.nextDouble();
+			double y = sc.nextDouble();
 			
+			if (x == 0.0 && y == 0.0) {
+				System.out.println("Origem");
+			} else if (x == 0) {
+				System.out.println("Eixo Y");
+			} else if (y == 0) {
+				System.out.println("Eixo X");
+			} else if (x > 0.0 && y > 0.0) {
+				System.out.println("Q1");
+			} else if (x < 0.0 && y > 0.0) {
+				System.out.println("Q2");
+			} else if (x < 0.0 && y < 0.0) {
+				System.out.println("Q3");
+			} else if (x > 0.0 && y < 0.0) {
+				System.out.println("Q4");
+			}
 		}
 	}
 }
