b9a22da8e0ee9c01e4f91fff940cb271d3a5bdcc
ezbueno <ezandrobueno@yahoo.com.br>
2021-12-28 12:55:59 -0300

    Exercícios resolvidos

---
+++ src/uri1035/Main.java
@@ -0,0 +1,24 @@
+package uri1035;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+			int d = sc.nextInt();
+			
+			int soma1 = c + d;
+			int soma2 = a + b;
+			
+			if (b > c && d > a && soma1 > soma2 && c >=0 && d >=0 && a % 2 == 0) {
+				System.out.println("Valores aceitos");
+			} else {
+				System.out.println("Valores nao aceitos");
+			}
+		}
+	}
+}
---
+++ src/uri1037/Main.java
@@ -0,0 +1,27 @@
+package uri1037;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
+		try (Scanner sc = new Scanner(System.in)) {
+			double valor = sc.nextDouble();
+							
+			if (valor >= 0 && valor <= 25.0) {
+				System.out.println("Intervalo [0,25]");
+			} else if (valor >= 25.01 && valor <= 50.0) {
+				System.out.println("Intervalo (25,50]");
+			} else if (valor >= 50.01 && valor <= 75.0) {
+				System.out.println("Intervalo (50,75]");
+			} else if (valor >= 75.01 && valor <= 100.0) {
+				System.out.println("Intervalo (75,100]");
+			} else {
+				System.out.println("Fora de intervalo");
+			}
+		}
+	}
+}
